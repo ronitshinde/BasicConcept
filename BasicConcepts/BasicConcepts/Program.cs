a4d601c5ae09e53eb4330fac389071164576ac82
ronitshinde <rony.shinde753@gmail.com>
2023-02-03 23:21:29 +0530

    [Ronit] Add : UC1-All Variables

---
+++ BasicConcepts/BasicConcepts/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicConcepts
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Variables.AllVariables();
+            Console.ReadLine();   
+        }
+    }
+}
---
+++ BasicConcepts/BasicConcepts/Variables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace BasicConcepts
+{
+    public class Variables
+    {
+        public static void AllVariables()
+        {
+            int myNum = 11;
+            double myDouble = 25.65D;
+            char myChar = 'A';
+            string myString = "Rise";
+            bool myBool = true;
+
+            Console.WriteLine(myNum);
+            Console.WriteLine(myDouble);
+            Console.WriteLine(myChar);
+            Console.WriteLine(myString);
+            Console.WriteLine(myBool);
+        }
+    }
+}
